1eab9edec0c919bce579cabf4859aeae19019078
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-09-03 22:27:44 +0300

    pre-merge commit

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "lennygdev",
+    "password": "Kenya123.",
+    "database": "postgres",
+    "host": "udagram-db-dev-lg.ciywow8027jr.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "alxt-udagram-lennyg-dev"
   },
   "jwt": {
     "secret": " "
